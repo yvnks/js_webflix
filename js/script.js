0f44ca2d430b0a1eb9034c9fd9fa328c0ce457a6
Jeff Yankson <jjyankson19@gmail.com>
2026-05-15 18:49:43 +0000

    Generate tv show details page with JS

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const global = {
 document.addEventListener("DOMContentLoaded", () => {
   switch (global.location) {
     case "/":
-    case "./index.html":
+    case "/index.html":
       console.log("Home");
       renderPopularMovies();
       break;
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Search");
       break;
     case "/tv-details.html":
+      displayShowDetails();
       console.log("tv details");
       break;
   }
@@ -106,7 +107,7 @@ async function renderPopularTVShows() {
   results.forEach((show) => {
     html += `
     <div class="card">
-          <a href="movie-details.html?id=${show.id}">
+          <a href="tv-details.html?id=${show.id}">
             ${
               show.poster_path
                 ? `<img src="https://tmdb.org/t/p/w500${show.poster_path}" 
@@ -201,6 +202,60 @@ function displayBackgroundImage(type, backdropImgPath) {
   overlayDiv.style.opacity = "0.1";
 
   document.body.appendChild(overlayDiv);
+}
+
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+
+  const show = await fetchDataFromAPI(`tv/${showId}`);
+  console.log(showId);
+
+  displayBackgroundImage("tv", show.backdrop_path);
+  const html = `
+  <div class="details-top">
+          <div>
+            ${
+              show.poster_path
+                ? `<img src="https://tmdb.org/t/p/w500${show.poster_path}" 
+                    class="card-img-top" alt="${show.name}"
+                 />`
+                : `<img src="images/no-image.jpg" 
+                    class="card-img-top" alt="show Title"
+                  />`
+            }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Seasons:</span> ${show.number_of_seasons}</li>
+            <li><span class="text-secondary">Episodes:</span> ${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air['name']} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(", ")}
+          </div>
+        </div>
+  `;
+  document.querySelector("#show-details").innerHTML = html;
 }
 
 function formatCurrency(num) {
